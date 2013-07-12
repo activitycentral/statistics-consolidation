24031810bc728847e2693ff89051f158edc184b5
Miguel Gonzalez <migonzalvar@activitycentral.com>
2013-07-12 20:50:07 +0200

    Remove py files generated by build

--- stats_consolidation/build/lib.linux-x86_64-2.7/stats_consolidation/test_cons.py
+++
@@ -1,9 +0,0 @@
-import stats_consolidation
-from stats_consolidation import *
-
-db = DB_Stats('statistics', 'root', 'gustavo')
-db.create();
-
-con = Consolidation('/home/gustavo/AC/server_stats/sugar-stats/rrd', db)
-
-con.process_rrds()
